5345ce304aaec703eae64ab532608ab79606ddf0
jalovisko <letovnn@gmail.com>
2023-04-04 09:45:26 -0400

    Revert "reverce"
    This reverts commit 4aed14b01555800c9cc59cfb3a92d7b88804f13a.

--- js/demo.js
+++ js/demo.js
@@ -6,8 +6,8 @@
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
     particleground(document.getElementById('particles'), {
-      dotColor: '#5cbdaa',
-      lineColor: '#5cbdaa'
+      dotColor: '#ED1B2F',
+      lineColor: '#ED1B2F'
     });
     var intro = document.getElementById('intro');
     intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
